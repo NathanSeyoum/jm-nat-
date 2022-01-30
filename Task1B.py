8eafd20a6047f28dcb3fdc3f1bb8baa0ddf572ff
James Mcallister <jamesmcallister@Jamess-Air.example.com>
2022-01-30 19:57:17 +0000

    an attempt at Task 1B test

--- Task1B.py
+++ Task1B.py
@@ -1,1 +1,15 @@
+print(-1)
+
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+
+print(0)
+data = build_station_list()
+print(1)
+stationsDistSorted = stations_by_distance(data, (52.2053, 0.1218))
+print(2)
+
+print(stationsDistSorted[:10])
+print(stationsDistSorted[-10:])
+
 
