4ae0795df7575d6aad05c34468858e30e42976e7
James Mcallister <jamesmcallister@dhcp-10-249-50-17.eduroam.wireless.private.cam.ac.uk>
2022-02-07 11:55:28 +0000

    task 1B

--- Task1B.py
+++ Task1B.py
@@ -1,15 +1,21 @@
-print(-1)
+#@james mcallister test for the geo station_by_distance function
+#test for task 1B 
+
+#probable issues I am missing a few modules that are needed in datafetcher.py
+
+#import dateutil.parser, import requests
 
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_distance
 
-print(0)
 data = build_station_list()
-print(1)
 stationsDistSorted = stations_by_distance(data, (52.2053, 0.1218))
-print(2)
+
 
 print(stationsDistSorted[:10])
 print(stationsDistSorted[-10:])
+
+#write assert tag
+
 
 
