37370fa189ec40fb73372a2d97d49747c4720852
James Mcallister <jamesmcallister@dhcp-10-249-50-17.eduroam.wireless.private.cam.ac.uk>
2022-02-07 12:14:59 +0000

    assertions

--- Task1C.py
+++ Task1C.py
@@ -1,1 +1,13 @@
 
+#@ james mcalliste write check function
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_within_radius
+
+data = build_station_list()
+stationDat = stations_within_radius(data,(52.2053, 0.1218))
+print(stationDat)
+
+#write assert tag
+assert(stationDat == ['Bin Brook', 'Cambridge Baits Bite', "Cambridge Byron's Pool",
+ 'Cambridge Jesus Lock', 'Comberton', 'Dernford', 'Girton',
+ 'Haslingfield Burnt Mill', 'Lode', 'Oakington', 'Stapleford'])
