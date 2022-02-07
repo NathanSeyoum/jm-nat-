b4363b13e4c84edfa57bc19e84f30ec713c1d304
Nathan Seyoum <ns854@cam.ac.uk>
2022-02-07 23:28:04 +0000

    making task B  work

--- Task1C.py
+++ Task1C.py
@@ -5,7 +5,9 @@ from floodsystem.geo import stations_within_radius
 
 data = build_station_list()
 stationDat = stations_within_radius(data,(52.2053, 0.1218),10)
-print(stationDat)
+
 
 #write assert tag
 assert(stationDat == ['Cambridge Jesus Lock', 'Bin Brook', "Cambridge Byron's Pool", 'Cambridge Baits Bite', 'Girton', 'Haslingfield Burnt Mill', 'Oakington', 'Stapleford', 'Comberton', 'Dernford', 'Lode'])
+
+print(stationDat)
