4957d2bb2cdf8a5157a2add2874cab529a9512bc
James Mcallister <jamesmcallister@Jamess-Air.example.com>
2022-02-14 23:57:31 +0000

    task 2C initial code

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -21,3 +21,18 @@ def stations_level_over_threshold(stations, tol): # by james mcallister task 2B
 
     return sortedList
 
+
+    def stations_highest_rel_level(stations, N):
+        
+
+        def byLevel(stat):#dictates how sorted
+            if stat.latest_level == None: #checks that latest value is real
+                return 0
+                #returns water value to sort with 
+            return stat.latest_level
+
+        #sorts stations by water level
+        sortedList = sorted(stations, key = byLevel, reverse=True)
+
+        #only returns correct number
+        return sortedList[:N]
