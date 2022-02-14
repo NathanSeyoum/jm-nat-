db3656cfe32da92d6b16fede4c4dd37a1e946193
James Mcallister <jamesmcallister@Jamess-Air.example.com>
2022-02-14 23:46:19 +0000

    task 2b

--- Task2B.py
+++ Task2B.py
@@ -2,6 +2,7 @@
 
 from floodsystem.stationdata import build_station_list
 from floodsystem.stationdata import update_water_levels
+from floodsystem.flood import stations_level_over_threshold
 
 def run():
     """Requirements for Task 2B"""
@@ -9,8 +10,12 @@ def run():
     # Build list of stations
     stations = build_station_list()
     update_water_levels(stations)
-    stationOne = stations[0]
-    print(stationOne.relative_water_level())
+    
+    ofendingStations = stations_level_over_threshold(stations, 0.8)
+
+    for items in ofendingStations: #takes offending stations and orders them into the correct locations
+        x,y = items
+        print(x.name,y)
    
 
 
