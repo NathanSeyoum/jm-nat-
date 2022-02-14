34399f89fd43804b852e2a8e28b24d7d8a0cf3a2
James Mcallister <jamesmcallister@Jamess-Air.example.com>
2022-02-14 23:46:07 +0000

    task 2b

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,1 +1,23 @@
-#death
+
+
+def stations_level_over_threshold(stations, tol): # by james mcallister task 2B
+    
+    returnList = []
+
+    # iterate through list to check against tolerances
+    for station in stations:
+        if station.latest_level == None:
+            break
+        elif station.latest_level > tol:
+            createTuple = (station,station.latest_level)
+            returnList.append(createTuple)
+
+    def byLevel(level):#dictates how sorted
+        x, lev = level
+        return lev
+
+    #sorts list
+    sortedList = sorted(returnList, key = byLevel, reverse=True)
+
+    return sortedList
+
