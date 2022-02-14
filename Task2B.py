70033e4c25e7117a0a8e32b0902e28f302ccb23d
James Mcallister <jamesmcallister@Jamess-Air.example.com>
2022-02-14 23:26:42 +0000

    task 2b part 1

--- Task2B.py
+++ Task2B.py
@@ -1,1 +1,19 @@
-#hwlll
+#test for task 2B
+
+from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import update_water_levels
+
+def run():
+    """Requirements for Task 2B"""
+
+    # Build list of stations
+    stations = build_station_list()
+    update_water_levels(stations)
+    stationOne = stations[0]
+    print(stationOne.relative_water_level())
+   
+
+
+if __name__ == "__main__":
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
+    run()
