3261c57ebe656c8c972cc6f212324d0fbc45ec22
James Mcallister <jamesmcallister@Jamess-Air.example.com>
2022-02-15 00:26:27 +0000

    task 2C

--- Task2C.py
+++ Task2C.py
@@ -1,1 +1,26 @@
-#death
+from re import S
+from floodsystem.stationdata import build_station_list
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.stationdata import update_water_levels
+
+def run():
+    """Requirements for Task 2C"""
+
+    # Build list of stations
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    topStations = stations_highest_rel_level(stations,10)
+
+    for items in topStations: #formats print
+        
+        print(items.name,items.relative_water_level())
+
+        #Letcombe Bassett seems slightly problimatic as relative level is 600+
+
+    
+
+
+if __name__ == "__main__":
+    print("*** Task 2C: CUED Part IA Flood Warning System ***")
+    run()
