729faddaf64f87db04659ace9f01f3222d8e9f7a
James Mcallister <jamesmcallister@Jamess-Air.example.com>
2022-02-15 00:27:09 +0000

    task 2c

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -6,10 +6,10 @@ def stations_level_over_threshold(stations, tol): # by james mcallister task 2B
 
     # iterate through list to check against tolerances
     for station in stations:
-        if station.latest_level == None:
+        if station.relative_water_level() == None:
             break
-        elif station.latest_level > tol:
-            createTuple = (station,station.latest_level)
+        elif station.relative_water_level() > tol:
+            createTuple = (station,station.relative_water_level())
             returnList.append(createTuple)
 
     def byLevel(level):#dictates how sorted
@@ -22,17 +22,20 @@ def stations_level_over_threshold(stations, tol): # by james mcallister task 2B
     return sortedList
 
 
-    def stations_highest_rel_level(stations, N):
+
+def stations_highest_rel_level(stations, N):
         
 
-        def byLevel(stat):#dictates how sorted
-            if stat.latest_level == None: #checks that latest value is real
-                return 0
-                #returns water value to sort with 
-            return stat.latest_level
+    def byLevel(stat):#dictates how sorted
+        if stat.relative_water_level() == None: #checks that latest value is real
+            return 0
+            #returns water value to sort with 
+        return stat.relative_water_level()
 
-        #sorts stations by water level
-        sortedList = sorted(stations, key = byLevel, reverse=True)
+    #sorts stations by water level
+    sortedList = sorted(stations, key = byLevel, reverse=True) 
 
-        #only returns correct number
-        return sortedList[:N]
+    #only returns correct number
+    return sortedList[:N]    
+    
+    # TODO: currently there is an issue with the relative_water_level function where it doesnt return an acceptable value
